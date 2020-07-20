e958bba20a9b61a6d244ad2fd75482389a546829
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-07-20 12:21:24 -0700

    image name: ecpe4s/rhel8-pantheon-base:YYYY-MM-DD

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-TE=`printf '%(%Y%m%d)T' -1`
+#!/bin/bash -x
+
+DATE=`printf '%(%Y-%m-%d)T' -1`
 REPO=`git config --get remote.origin.url`
 COMMIT=`git log -1 --format="%H"`
 
@@ -6,4 +8,4 @@ docker build \
   --build-arg DATE=${DATE} \
   --build-arg REPO=${REPO} \
   --build-arg COMMIT=${COMMIT} \
-  -t ecpe4s/rhel8-base-x86_64:${DATE} .
+  -t ecpe4s/rhel8-pantheon-base:${DATE} .
